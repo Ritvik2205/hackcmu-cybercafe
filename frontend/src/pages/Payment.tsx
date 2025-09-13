2d6f96f992969279d811d79f9df33d701c0bcda7
Saul Alvarez <salvarez98@hotmail.com>
2025-09-13 15:08:25 -0400

    payments

--- frontend/src/pages/Payment.tsx
+++ frontend/src/pages/Payment.tsx
@@ -2,6 +2,7 @@ import { SignUp, SignedOut, SignedIn } from '@clerk/clerk-react'
 import { useNavigate } from 'react-router-dom';
 import React, { useRef, useState, useEffect} from 'react';
 import axios from 'axios';
+import { useAuth } from "@clerk/clerk-react";
 
 interface TextFieldProps {
   value: string;
@@ -29,10 +30,49 @@ const TextField= ({ onChange, value, ...rest }: TextFieldProps) => {
 
 const Payment: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [creditCard, setCreditCard] = useState<string>("");
+  const [CVV, setCVV] = useState<string>("");
+  const [expDate, setexpDate] = useState<string>("");
   
   const textChange=((newVal:string) => {
     setInputValue(newVal)
-  })
+  });
+  const textChangeCredit=((newVal:string) => {
+    setCreditCard(newVal)
+  });
+  const textChangeCVV=((newVal:string) => {
+    setCVV(newVal)
+  });
+  const textChangeexp=((newVal:string)=>{
+    setexpDate(newVal)
+  });
+
+  const { getToken } = useAuth();
+
+  const processPayment=(async () =>{
+    const clerkToken = (await getToken()) || "";
+    try {
+        const response = await fetch('http://localhost:8000/add_credit/', {
+          method: 'POST',
+          // mode: "no-cors",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ "account": clerkToken, "quantity": inputValue }), // Data to send
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Success:', data);
+        // Handle successful response (e.g., update state, show message)
+      } catch (error) {
+        console.error('Error during POST request:', error);
+        // Handle error (e.g., show error message)
+      }
+  });
 
   return (<section>
     <div className="product">
@@ -46,12 +86,25 @@ const Payment: React.FC = () => {
       value={inputValue}
       onChange={textChange}
       type='text'/>
+      <TextField
+      id='credit_card'
+      value={creditCard}
+      onChange={textChangeCredit}
+      type='text'/>
+      <TextField
+      id='cvv'
+      value={CVV}
+      onChange={textChangeCVV}
+      type='text'/>
+      <TextField
+      id='exp_date'
+      value={expDate}
+      onChange={textChangeexp}
+      type='text'/>
     </div>
-    <form action="/create-checkout-session" method="POST">
-      <button type="submit">
-        Checkout
-      </button>
-    </form>
+    <button type="submit" onClick={processPayment}>
+    </button>
+    <button type="back"></button>
   </section>);
 };
 
